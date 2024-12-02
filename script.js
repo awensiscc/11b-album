d624cd01fb55181cc8fb807eae54a041b8cc0552
Mykyta Doroshenko <nikitadoroshenko50@gmail.com>
2024-12-02 11:31:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const groupPhotos = [
         'photo1.jpg',
         'photo2.jpg',
-        'photo3spring.jpg',
+        'photo3.jpg',
+        'photo4.jpg',
+        'photo5.jpg',
+        'photo6.jpg',
+        'photo7.jpg',
+        'photo8.jpg',
+        'photo9.jpg',
+        'photo10.jpg',
+        'photo11.jpg',
         // Добавьте остальные фотографии
     ];
 
